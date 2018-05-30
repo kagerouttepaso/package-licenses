7a067f166922ba3cb7e5abc02ddd5d051f4b5435
reniris <nyoronu@gmail.com>
2018-05-30 21:02:52 +0900

    コマンドラインオプションでプロジェクトファイルを設定可能に

--- PackageReferenceSample/Program.cs
+++ PackageReferenceSample/Program.cs
@@ -12,107 +12,47 @@ using NuGet.Packaging.Core;
 
 namespace PackageReferenceSample
 {
-    class Logger : ILogger
-    {
-        public void Log(LogLevel level, string data) => $"{level.ToString().ToUpper()}: {data}".Dump();
-        public void Log(ILogMessage message) => Task.FromResult(0);
-        public Task LogAsync(LogLevel level, string data) => Task.FromResult(0);
-        public Task LogAsync(ILogMessage message) => throw new NotImplementedException();
-        public void LogDebug(string data) => $"DEBUG: {data}".Dump();
-        public void LogError(string data) => $"ERROR: {data}".Dump();
-        public void LogInformation(string data) => $"INFORMATION: {data}".Dump();
-        public void LogInformationSummary(string data) => $"SUMMARY: {data}".Dump();
-        public void LogMinimal(string data) => $"MINIMAL: {data}".Dump();
-        public void LogVerbose(string data) => $"VERBOSE: {data}".Dump();
-        public void LogWarning(string data) => $"WARNING: {data}".Dump();
-    }
+	class Logger : ILogger
+	{
+		public Action<string> Write { get; set; } = Console.WriteLine;
 
-    static class LogExtension
-    {
-        public static void Dump(this string value) => Console.WriteLine(value);
-    }
+		public void Log(LogLevel level, string data) { }
+		public void Log(ILogMessage message) { }
+		public System.Threading.Tasks.Task LogAsync(LogLevel level, string data) => System.Threading.Tasks.Task.FromResult(0);
+		public System.Threading.Tasks.Task LogAsync(ILogMessage message) => System.Threading.Tasks.Task.FromResult(0);
+		public void LogDebug(string data) => Write?.Invoke($"DEBUG: {data}\n");
+		public void LogError(string data) => Write?.Invoke($"ERROR: {data}\n");
+		public void LogInformation(string data) => Write?.Invoke($"INFORMATION: {data}\n");
+		public void LogInformationSummary(string data) => Write?.Invoke($"SUMMARY: {data}\n");
+		public void LogMinimal(string data) => Write?.Invoke($"MINIMAL: {data}\n");
+		public void LogVerbose(string data) => Write?.Invoke($"VERBOSE: {data}\n");
+		public void LogWarning(string data) => Write?.Invoke($"WARNING: {data}\n");
+	}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var log = new Logger();
-            const string samplefile = "PackageReferenceSample.csproj.xml";
-            var dir = System.AppDomain.CurrentDomain.BaseDirectory;
-            var fullpath = Path.Combine(dir, samplefile);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var log = new Logger();
+			const string samplefile = "PackageReferenceSample.csproj.xml";
+			var dir = System.AppDomain.CurrentDomain.BaseDirectory;
+			var fullpath = Path.Combine(dir, samplefile);
 
-            var packages = GetPackages(fullpath, log);
+			// Create Output folder
+			var savefolder = Path.Combine(dir, $"{Path.GetFileName("Licenses")}-{DateTime.Now.ToFileTimeUtc()}");
+			Directory.CreateDirectory(savefolder);
 
-            var list = new List<(LocalPackageInfo, License)>();
-            var t = Task.Run(async () =>
-            {
-                foreach (var p in packages)
-                {
-                    var license = await p.GetLicenseAsync(log);
-                    list.Add((p, license));
-                }
-            });
-            t.Wait();
+			var project = args.Length > 0 ? args[0] : fullpath;
+			// List
+			var result = PackageLicensesUtility.TryPackageReferencesListAsync(project, savefolder, log);
 
-            DispData(list);
-            Console.WriteLine("Completed.");
+			if (result.Result == false)
+				Directory.Delete(savefolder, true);
+
+			Console.WriteLine("Completed.");
 #if DEBUG
-            Console.ReadKey();
+			Console.ReadKey();
 #endif
-        }
-
-        private static List<LocalPackageInfo> GetPackages(string fullpath, Logger log)
-        {
-
-
-            var pkg = @"%UserProfile%\.nuget\packages";
-
-            XDocument xml = XDocument.Load(fullpath);
-            var result = xml.Element("Project").Elements("ItemGroup")
-                .Where(i => i.Element("PackageReference") != null).Select(i => i.Elements("PackageReference")).First();
-
-            var packages = new List<LocalPackageInfo>();
-
-            foreach (var item in result)
-            {
-                var inc = item.Attribute("Include");
-                var ver = item.Attribute("Version");
-
-                var nuspec = Environment.ExpandEnvironmentVariables(Path.Combine(pkg, inc.Value, ver.Value, inc.Value + "." + ver.Value + ".nupkg"));
-
-                packages.Add(LocalFolderUtility.GetPackage(new Uri(nuspec), log));
-            }
-
-            return packages;
-        }
-
-        private static void DispData(List<(LocalPackageInfo, License)> list)
-        {
-            // header
-            var headers = new[] { "Id", "Version", "Authors", "Title", "ProjectUrl", "LicenseUrl", "RequireLicenseAcceptance", "Copyright", "Inferred License ID", "Inferred License Name" };
-            for (var i = 0; i < headers.Length; i++)
-            {
-                Console.WriteLine(String.Join('\t',headers));
-            }
-
-            // values
-            foreach (var (p, l) in list)
-            {
-                var nuspec = p.Nuspec;
-                var text = new [] {
-                    nuspec.GetId() ?? "",
-                    $"{nuspec.GetVersion()}",
-                    nuspec.GetAuthors() ?? "",
-                    nuspec.GetTitle() ?? "",
-                    nuspec.GetProjectUrl() ?? "",
-                    nuspec.GetLicenseUrl() ?? "",
-                    $"{nuspec.GetRequireLicenseAcceptance()}",
-                    nuspec.GetCopyright() ?? "",
-                    l?.Id ?? "",
-                    l?.Name ?? ""};
-
-                Console.WriteLine(String.Join('\t',text));
-            }
-        }
-    }
+		}
+	}
 }
